7fd6e23c73ec9112a333b107c42aa9454a68eba3
Emmanuela Opurum <emmanuela_prince@yahoo.com>
2025-11-28 21:15:45 +0000

    update .tf backend

--- main.tf
+++ main.tf
@@ -1,44 +1,27 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_s3_bucket" "tf_state" {
   bucket = var.state_bucket_name
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   lifecycle {
     prevent_destroy = true
   }
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = var.lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# Versioning must be a separate resource
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
 
-  attribute {
-    name = "LockID"
-    type = "S"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Encryption also should be separate
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
-variable "region" {}
-variable "state_bucket_name" {}
-variable "lock_table_name" {}
+variable "region" {
+  description = "AWS region where the backend resources will be created"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "state_bucket_name" {
+  description = "Name of the S3 bucket to store Terraform remote state"
+  type        = string
+  default     = "tfstate-cloud-architect-emma"
+}
+
+variable "lock_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
+  default     = "tf-lock-cloud-architect-emma"
+}
